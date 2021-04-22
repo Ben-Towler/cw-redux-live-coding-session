29670394d00df36be139f62025850e3bd21cb912
Ben Towler <ben.towler.54@gmail.com>
2021-04-21 21:17:17 -0400

    feat: added basic redux example

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '../lib/redux';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const store = configureStore();
 
-export default MyApp
+const App = ({ Component, pageProps }) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
+
+export default App
